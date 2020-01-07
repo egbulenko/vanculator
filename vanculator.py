61a9c6fae4f65751fa69858822cfe42a516db72d
Egor Bulenko <egbulenko@gmail.com>
2020-01-07 15:50:40 +0700

    (изучение python) как задать три вопроса подряд

--- vanculator.py
+++ vanculator.py
@@ -1,8 +1,28 @@
+# 2. Задавать определённое количество вопросов (например, 3) и после этого останавливаться
+
 print("5 + 10 = ?")
 print("Введите ответ:")
 ответ = '15'
 perremmeennaayyaa = input()
 if perremmeennaayyaa == ответ:
+    print("Отлично!")
+else:
+    print("НЕ ТАК!!!")
+
+print("12 + 45 = ?")
+print("Введите ответ:")
+otvet = '57'
+perremmeennaayya = input()
+if perremmeennaayya == otvet:
+    print("Отлично!")
+else:
+    print("НЕ ТАК!!!")
+
+print("0 + 0 = ?")
+print("Введите ответ:")
+otvetik = '00'
+perremmeennaaya = input()
+if perremmeennaaya == otvetik:
     print("Отлично!")
 else:
     print("НЕ ТАК!!!")
